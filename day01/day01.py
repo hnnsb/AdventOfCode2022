804f13d329aef682b5c2ae3e41029ba199678823
stu227506 <stu227506@mail.uni-kiel.de>
2022-12-01 17:51:13 +0100

    Day01

---
+++ day01/day01.py
@@ -0,0 +1,16 @@
+
+
+
+with open("day01/input.txt", "r") as f:
+    data = []
+    sub = []
+    for row in f:
+        if row == '\n':
+            data.append(sub)
+            sub = []
+        else:
+            sub.append(int(row.strip('\n')))
+
+sums =  [sum(food) for food in data]
+print("Part 1: ", max(sums))
+print("Part 2: ", sum(sorted(sums)[-3:]))
