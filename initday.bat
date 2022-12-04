8d661cd12890441b4b62337ef01911c808794cab
hnnsb <hannes.b@wtnet.de>
2022-12-04 20:59:27 +0100

    moved browser opening to init script

--- initday.bat
+++ initday.bat
@@ -4,6 +4,9 @@ IF ["%1"]==[""] GOTO ERROR
 :CREATE
 MKDIR day%1
 COPY "templates\template2.py" "day%1\day%1.py"
+set num=%1
+set num=%num:0=%
+START "" https://adventofcode.com/2022/day/%num%
 echo Successfully created Day %1
 GOTO END
 
--- templates/template2.py
+++ templates/template2.py
@@ -3,14 +3,12 @@ from re import findall
 from dotenv import load_dotenv
 from dotenv import dotenv_values
 load_dotenv()
-import webbrowser
 from aocd.models import Puzzle
 from aocd import submit
 
 
 fileName = basename(__file__)
 day = int(findall(r"[1-9]+",fileName)[0])
-webbrowser.open(f'https://adventofcode.com/2022/day/{day}', new=1)
 # -----------------------------------------------------
 
 puzzle = Puzzle(day=day, year=dotenv_values()["YEAR"])
