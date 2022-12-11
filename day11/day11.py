76401bc7eeaf679019f80ef8f7da6799f374c13e
hnnsb <hannes.b@wtnet.de>
2022-12-11 20:36:55 +0100

    Cleanup Day 11

--- day11/day11.py
+++ day11/day11.py
@@ -1,9 +1,9 @@
+from copy import deepcopy
 from aocd.models import Puzzle
 from os.path import basename
 from re import findall
 from dotenv import load_dotenv
 from dotenv import dotenv_values
-import math
 load_dotenv()
 
 fileName = basename(__file__)
@@ -15,24 +15,6 @@ data = puzzle.input_data.split("\n\n")
 if TEST:
     with open(f"day{day}/test.txt", "r") as f:
         data = f.read().split("\n\n")
-    operations = [
-        lambda x: x * 19,
-        lambda x: x + 6,
-        lambda x: x * x,
-        lambda x: x + 3,
-    ]
-    
-else:
-    operations = [
-        lambda x: x * 7,
-        lambda x: x + 4,
-        lambda x: x + 2,
-        lambda x: x + 7,
-        lambda x: x * 17,
-        lambda x: x + 8,
-        lambda x: x + 6,
-        lambda x: x * x,
-    ]
 
 data = [monkey.split("\n") for monkey in data]
 monkeys = []
@@ -40,37 +22,50 @@ for i, m in enumerate(data):
     monkey = {}
     items = m[1].split(": ")[1].split(", ")
     monkey["items"] = [int(item) for item in items]
+    operation = " ".join(m[2].split(" ")[-3:])
+    monkey["operation"] = lambda old, operation=operation: eval(operation)
     monkey["test"] = int(m[3].split(" ")[-1])
     monkey[True] = int(m[4].split(" ")[-1])
     monkey[False] = int(m[5].split(" ")[-1])
     monkey["inspected"] = 0
     monkeys.append(monkey)
 
-product = 2*3*5*7*11*13*17*19
-part1 = False
-if part1:
-    limit = 20
-else:
-    limit = 10000
-for round in range(limit):
+monkeys2 = deepcopy(monkeys)
+
+for round in range(20):
     for i, monkey in enumerate(monkeys):
         while len(monkey["items"]) > 0:
             item = monkey["items"].pop(0)
             monkey["inspected"] += 1
-            worrylevel = operations[i](item) % product
-            if part1:
-                worrylevel = worrylevel // 3
+            worrylevel = monkey["operation"](item) // 3
             test = worrylevel % monkey["test"] == 0
-
             monkeys[monkey[test]]["items"].append(worrylevel)
 
 maxInspec = []
 for monkey in monkeys:
     maxInspec.append(monkey["inspected"])
 maxInspec = sorted(maxInspec)
-print(maxInspec)
-print(maxInspec[-1] * maxInspec[-2])
-if part1:
-    puzzle.answer_a = maxInspec[-1] * maxInspec[-2]
-else:
-    puzzle.answer_b = maxInspec[-1] * maxInspec[-2]
+print("Part 1: ", maxInspec[-1] * maxInspec[-2])
+puzzle.answer_a = maxInspec[-1] * maxInspec[-2]
+
+
+# Part 2
+commonMultiple = 1
+for monkey in monkeys:
+    commonMultiple *= monkey["test"]
+
+for round in range(10000):
+    for i, monkey in enumerate(monkeys2):
+        while len(monkey["items"]) > 0:
+            item = monkey["items"].pop(0)
+            monkey["inspected"] += 1
+            worrylevel = monkey["operation"](item) % commonMultiple
+            test = worrylevel % monkey["test"] == 0
+            monkeys2[monkey[test]]["items"].append(worrylevel)
+
+maxInspec = []
+for monkey in monkeys2:
+    maxInspec.append(monkey["inspected"])
+maxInspec = sorted(maxInspec)
+print("Part 2: ", maxInspec[-1] * maxInspec[-2])
+puzzle.answer_b = maxInspec[-1] * maxInspec[-2]
